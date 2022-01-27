1883ea6ba39f7884c535baad8fb32c12836e5bbd
Marcelo Ferreira <flaviomff@gmail.com>
2022-01-27 17:49:58 -0300

    chore: adding bootstrap styles

--- src/components/VslSection.tsx
+++ src/components/VslSection.tsx
@@ -1,22 +1,27 @@
 import styled from 'styled-components';
+import { Col, Container } from 'react-bootstrap';
 import PlyrPlayer from './PlyrPlayer';
 
 const VslSection = () => (
-  <Container>
+  <Wrapper>
     <h2>Chegou a hora de tirar as ideias do papel</h2>
     <p>Pensou em algum negócio?</p>
-    <p>Coloquem em prática</p>
-    <PlyrPlayer
-      url="https://www.youtube.com/watch?v=0Eub-aQs-44"
-      onTimeUpdate={e => console.log(e.detail.plyr.currentTime)}
-      onPlay={e => console.log(e)}
-      onPause={e => console.log(e)}
-      autoPlay
-    />
-  </Container>
+    <p>Coloque em prática</p>
+    <Container className="d-flex justify-content-center">
+      <Col xl="10" xs>
+        <PlyrPlayer
+          url="https://www.youtube.com/watch?v=0Eub-aQs-44"
+          onTimeUpdate={e => console.log(e.detail.plyr.currentTime)}
+          onPlay={e => console.log(e)}
+          onPause={e => console.log(e)}
+          autoPlay
+        />
+      </Col>
+    </Container>
+  </Wrapper>
 );
 
-const Container = styled.div`
+const Wrapper = styled.div`
   background-color: #0b132b;
   color: #fff;
   text-align: center;
