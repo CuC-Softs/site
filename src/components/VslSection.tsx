0baacfd7dbb0e186f537dc559c41e788c6ec8412
Marcelo Ferreira <flaviomff@gmail.com>
2022-01-27 16:56:03 -0300

    chore: changing VslSection styles with styled components

--- src/components/VslSection.tsx
+++ src/components/VslSection.tsx
@@ -1,14 +1,26 @@
+import styled from 'styled-components';
 import PlyrPlayer from './PlyrPlayer';
 
 const VslSection = () => (
-  <div>
+  <Container>
+    <h2>Chegou a hora de tirar as ideias do papel</h2>
+    <p>Pensou em algum negócio?</p>
+    <p>Coloquem em prática</p>
     <PlyrPlayer
       url="https://www.youtube.com/watch?v=0Eub-aQs-44"
       onTimeUpdate={e => console.log(e.detail.plyr.currentTime)}
       onPlay={e => console.log(e)}
       onPause={e => console.log(e)}
+      autoPlay
     />
-  </div>
+  </Container>
 );
+
+const Container = styled.div`
+  background-color: #0b132b;
+  color: #fff;
+  text-align: center;
+  /* padding: 20px 100px; */
+`;
 
 export default VslSection;
