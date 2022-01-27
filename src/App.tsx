573236e6d7dbd6487e7d0828c263e60545809fb1
Marcelo Ferreira <flaviomff@gmail.com>
2022-01-27 16:38:11 -0300

    feat: adding global styles to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
 import theme from './styles/theme';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <ThemeProvider theme={theme}>
@@ -10,6 +11,7 @@ const App: React.FC = () => (
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      <GlobalStyle />
     </div>
   </ThemeProvider>
 );
