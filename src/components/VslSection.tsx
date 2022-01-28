555c81e35781899426ed4dd0408d51943f25df03
Marcelo Ferreira <flaviomff@gmail.com>
2022-01-28 18:09:49 -0300

    chore: cleaning VslSection

--- src/components/VslSection.tsx
+++ src/components/VslSection.tsx
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
 import { Col, Container } from 'react-bootstrap';
 import PlyrPlayer, { PLYR_CONTROLS } from './PlyrPlayer';
+import BigFragmentsGroup from './fragments/BigFragmentsGroup';
+import FragmentGroupOne from './fragments/FragmentGroupOne';
+import FragmentGroupTwo from './fragments/FragmentGroupTwo';
+import FragmentGroupThree from './fragments/FragmentGroupThree';
+import FragmentGroupFour from './fragments/FragmentGroupFour';
 
 const VslSection = () => (
   <Wrapper>
-    <h2>Chegou a hora de tirar as ideias do papel</h2>
-    <Container className="d-flex justify-content-center p-2">
+    <Container className="d-flex flex-column justify-content-center align-items-center p-4 position-relative">
+      <TextBrand className="pb-5">
+        <h2>Chegou a hora de tirar as ideias do papel!</h2>
+      </TextBrand>
+      <BigFragmentsGroup />
+      <FragmentGroupOne />
+      <FragmentGroupTwo />
+      <FragmentGroupThree />
+      <FragmentGroupFour />
       <Col xl="10" xs>
         <PlyrPlayer
           url="https://www.youtube.com/embed/4lEeSwgY4XA?rel=0&amp;modestbranding=1"
@@ -23,6 +35,16 @@ const Wrapper = styled.div`
   color: #fff;
   text-align: center;
   padding: 20px 0px;
+`;
+
+const TextBrand = styled.div`
+  width: 600px;
+  h2 {
+    font-size: 48px;
+    font-weight: bold;
+    line-height: 72px;
+  }
+  z-index: 1;
 `;
 
 export default VslSection;
