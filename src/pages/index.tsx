7e1aaf9b0c19874b238beb8e0f8367822b3e465f
Manuel-Antunes <nerdbroz2@gmail.com>
2022-01-30 16:07:35 -0300

    chore: add vsl section to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import VslSection from '../components/VslSection';
 
 function IndexPage() {
   return (
-    <Box
-      w="100vw"
-      h="100vh"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      bgColor="yellow.300"
-    >
-      <Text fontSize="64px" fontWeight="bold" color="white">
-        Hello world!
-      </Text>
+    <Box width="100%">
+      <VslSection />
     </Box>
   );
 }
