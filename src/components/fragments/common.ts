664596f371d60bc4351e1eda79978a1f10a71497
Manuel-Antunes <nerdbroz2@gmail.com>
2022-01-30 16:06:25 -0300

    chore: turn all animations to responsiveness

--- src/components/fragments/common.ts
+++ src/components/fragments/common.ts
@@ -1,4 +1,4 @@
-import { keyframes } from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const bounce = keyframes`
   0%, 20%, 40%, 60%, 80%, 100% {transform: translateY(0);}
@@ -11,23 +11,29 @@ export const vibrate = (position: number) => keyframes`
             transform: translate(0);
   }
   20% {
-    -webkit-transform: translate(-${position}px, ${position}px);
-            transform: translate(-${position}px, ${position}px);
+    -webkit-transform: translate(-${position}%, ${position}%);
+            transform: translate(-${position}%, ${position}%);
   }
   40% {
-    -webkit-transform: translate(-${position}px, -${position}px);
-            transform: translate(-${position}px, -${position}px);
+    -webkit-transform: translate(-${position}%, -${position}%);
+            transform: translate(-${position}%, -${position}%);
   }
   60% {
-    -webkit-transform: translate(${position}px, ${position}px);
-            transform: translate(${position}px, ${position}px);
+    -webkit-transform: translate(${position}%, ${position}%);
+            transform: translate(${position}%, ${position}%);
   }
   80% {
-    -webkit-transform: translate(${position}px, -${position}px);
-            transform: translate(${position}px, -${position}px);
+    -webkit-transform: translate(${position}%, -${position}%);
+            transform: translate(${position}%, -${position}%);
   }
   100% {
     -webkit-transform: translate(0);
             transform: translate(0);
   }
+`;
+
+export const FragmentContainer = styled.div`
+  position: relative;
+  weight: 100%;
+  height: 100%;
 `;
