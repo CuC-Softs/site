a7ce80927044c0debb10526c08e07215aae23984
Marcelo Ferreira <flaviomff@gmail.com>
2022-02-03 18:01:36 -0300

    feat: adding google-analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
 module.exports = {
     siteMetadata: {
       title: `site`,
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://xn--usofts-vuab.com.br`,
     },
     plugins: [
+      {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          // You can add multiple tracking ids and a pageview event will be fired for all of them.
+          trackingIds: [
+            "G-2RWT1FNNGT", // Google Analytics / GA
+          ],
+          // This object is used for configuration specific to this plugin
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: true,
+          },
+        },
+      },
       '@chakra-ui/gatsby-plugin',
       {
         resolve: `gatsby-plugin-typescript`,
