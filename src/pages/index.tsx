ad06fd385226092da5c07705ad99c77f26438b00
Marcelo Ferreira <flaviomff@gmail.com>
2022-02-08 11:20:53 -0300

    feat: importing react pixel, adding pixel id and adding pageView tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
 import { Box } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import VslSection from '../components/VslSection';
 
 function IndexPage() {
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then(x => x.default)
+      .then(ReactPixelFB => {
+        if (typeof window !== 'undefined') {
+          const ReactPixel = ReactPixelFB;
+          ReactPixel.init('249251460695363');
+          ReactPixel.pageView();
+        }
+      });
+  }, []);
   return (
     <Box width="100%">
       <VslSection />
