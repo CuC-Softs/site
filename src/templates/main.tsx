4ae7af45553bcb7c2e14dc887d9942a13e352e08
Marcelo Ferreira <flaviomff@gmail.com>
2022-02-09 10:32:57 -0300

    fix: removing context api

--- src/templates/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ReactPixelProvider } from '../contexts/FacebookPixelContext';
-
-const MainTemplate = ({ children }) => (
-  <ReactPixelProvider>{children}</ReactPixelProvider>
-);
-export default MainTemplate;
