e0b62f0346a66ad3227724e282f6be6b1d192fcf
Marcelo Ferreira <flaviomff@gmail.com>
2022-03-16 16:10:15 -0300

    feat: creating team page and adding layout template to it

---
+++ src/pages/teampage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../templates/Layout';
+
+function TeamPage() {
+  return (
+    <Layout>
+      <section>
+        <h1>Team page</h1>
+      </section>
+    </Layout>
+  );
+}
+
+export default TeamPage;
