fb87b7bd7788a4c788897a6c8b7bf8146bae8667
Marcelo Ferreira <flaviomff@gmail.com>
2022-03-22 11:40:46 -0300

    feat: creating technologies section structure

---
+++ src/components/tecnologies/TecnologiesSection.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+// import ReactSvg from './ReactSvg';
+import ReactLogo from './ReactLogo.png';
+
+function TecnologiesSection() {
+  return (
+    <section style={{ backgroundColor: '#131313' }}>
+      <div className="container">
+        <div className="row mx-auto mb-5 pt-5 ps-5">
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mx-auto mb-5 pb-5 ps-5">
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-4">
+            <div
+              className="card text-center text-light"
+              style={{ width: '15rem', backgroundColor: '#2C2C2C' }}
+            >
+              <div className="card-body">
+                <img
+                  src={ReactLogo}
+                  alt="tech-logo"
+                  style={{ width: '10em' }}
+                />
+
+                <h5 className="card-title">ReactJS</h5>
+                <p className="card-text" style={{ fontSize: '12px' }}>
+                  O React é uma biblioteca JavaScript de código aberto com foco
+                  em criar interfaces de usuário em páginas web.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default TecnologiesSection;
