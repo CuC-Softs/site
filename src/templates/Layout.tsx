e73f50dc8923c55c0361d9d8a93dfc2d71213c60
Marcelo Ferreira <flaviomff@gmail.com>
2022-03-29 10:09:35 -0300

    chore: add global style

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createGlobalStyle } from 'styled-components';
+import Navbar from '../components/common/Navbar';
 
 function Layout({ children }) {
   return (
-    <div>
-      <nav>
-        <h1>Navbar</h1>
-      </nav>
+    <>
+      <GlobalStyle />
+      <Navbar />
       <div>{children}</div>
       <footer>
         <h1>Footer</h1>
       </footer>
-    </div>
+    </>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+    font-family: 'Ubuntu';
+  }
+  body{
+    -webkit-font-smoothing: antialiased;
+  }
+`;
 
 export default Layout;
