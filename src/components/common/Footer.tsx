6bf5d5580d10d739892335b6dbc21b4db759ce8d
Marcelo Ferreira <flaviomff@gmail.com>
2022-03-31 11:07:01 -0300

    feat: create footer

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import CucLogo from './CucLogo';
+import GithubVector from './social_vectors/GithubVector';
+import InstagramVector from './social_vectors/InstagramVector';
+import LinkedinVector from './social_vectors/LinkedinVector';
+
+function Footer() {
+  return (
+    <StyledFooter>
+      <StyledBrand>
+        <StyledBrandName>
+          <CucLogo />
+          <h3>ÇUÇ</h3>
+        </StyledBrandName>
+        <div>
+          <p style={{ marginBottom: '15px' }}>&copy; ÇuÇ Softs 2022</p>
+          <p>Todos os direitos reservados</p>
+        </div>
+      </StyledBrand>
+      <StyledIcons>
+        <GithubVector />
+        <InstagramVector />
+        <LinkedinVector />
+      </StyledIcons>
+    </StyledFooter>
+  );
+}
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #131313;
+  padding: 75px 100px;
+  color: #fff;
+`;
+const StyledBrand = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledBrandName = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const StyledIcons = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export default Footer;
