e9f4d55f424a37910dad767aea33b362beb01ceb
Marcelo Ferreira <flaviomff@gmail.com>
2022-03-31 10:34:30 -0300

    feat: add tech section to the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Layout from '../templates/Layout';
 import HeroSection from '../components/index_page/HeroSection';
+import TechnologiesSection from '../components/index_page/TechnologiesSection';
 
 function IndexPage() {
   return (
     <Layout>
       <HeroSection />
+      <TechnologiesSection />
     </Layout>
   );
 }
